3a6746055cd24217d911edcbf01c75be557a8a35
dashhyphenminus <robertblendea@outlook.com>
2023-10-23 15:02:56 -0700

    import meepmeep, CHECK SETTINGS.GRADLE, YOU MAY NEED TO REFRESH THAT FOR IT TO WORK 2 electric boogaloo

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -26,7 +26,12 @@ public class MeepMeepTesting {
                                 .splineToSplineHeading(BLUE_WAYPOINT_1, Math.toRadians(-45))
                                 .splineToSplineHeading(BLUE_WAYPOINT_2, Math.toRadians(45))
                                 .splineToSplineHeading(BLUE_BACKDROP_LEFT, Math.toRadians(0))
+                                .splineToSplineHeading(BLUE_BACKDROP_LEFT, Math.toRadians(0))
+                                .splineToSplineHeading(BLUE_BACKDROP_LEFT, Math.toRadians(0))
+                                .splineToSplineHeading(BLUE_BACKDROP_LEFT, Math.toRadians(0))
+
                                 .build()
+
                 );
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_OFFICIAL)
