b70e67c31575397cfeb4938a6e59aa27876c19a5
Ved Anand <ved.anand@outlook.com>
2023-10-25 15:54:49 -0700

    rearrange files

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TestOpModes/AirplaneLaunch.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TestOpModes/AirplaneLaunch.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.opmodes.teleop;
+package org.firstinspires.ftc.teamcode.opmodes.TestOpModes;
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TestOpModes/TensorFlowTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TestOpModes/TensorFlowTest.java
@@ -27,7 +27,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package org.firstinspires.ftc.teamcode.opmodes;
+package org.firstinspires.ftc.teamcode.opmodes.TestOpModes;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
