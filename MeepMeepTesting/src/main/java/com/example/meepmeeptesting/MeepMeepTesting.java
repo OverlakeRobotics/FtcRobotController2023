a49c51d4b7d98f4a28ddb76604c7dfa0ddc5fcc8
dashhyphenminus <robertblendea@outlook.com>
2023-10-31 11:15:16 -0700

    tuning mostly done, developing paths

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -16,20 +16,12 @@ public class MeepMeepTesting {
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .setConstraints(30, 30, Math.toRadians(180), Math.toRadians(180), 15)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(BLUE_START_POS_2)
-                                .splineToSplineHeading(BLUE_OBJECT_POS_4_1, Math.toRadians(-90))
-                                .splineToSplineHeading(BLUE_OBJECT_POS_4_2, Math.toRadians(-30))
+                        drive.trajectorySequenceBuilder(BLUE_START_POS_1)
+                                .splineToSplineHeading(BLUE_OBJECT_POS_1, Math.toRadians (0))
                                 .waitSeconds(1)
-                                .splineToSplineHeading(BLUE_OBJECT_POS_4_3, Math.toRadians(135))
-                                .splineToSplineHeading(BLUE_WAYPOINT_1, Math.toRadians(-45))
-                                .splineToSplineHeading(BLUE_WAYPOINT_2, Math.toRadians(45))
-                                .splineToSplineHeading(BLUE_BACKDROP_LEFT, Math.toRadians(0))
-                                .splineToSplineHeading(BLUE_BACKDROP_LEFT, Math.toRadians(0))
-                                .splineToSplineHeading(BLUE_BACKDROP_LEFT, Math.toRadians(0))
-                                .splineToSplineHeading(BLUE_BACKDROP_LEFT, Math.toRadians(0))
-
+                                .splineToSplineHeading(BLUE_BACKDROP_LEFT, Math.toRadians(180))
                                 .build()
 
                 );
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
@@ -31,12 +31,12 @@ public class StandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer 
     public static double WHEEL_RADIUS = 0.75d; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (encoder) speed
 
-    public static double LATERAL_DISTANCE = 14.895; // in; distance between the left and right wheels
-    public static double FORWARD_OFFSET = 7; // in; offset of the lateral wheel
+    public static double LATERAL_DISTANCE = 15.68; // in; distance between the left and right wheels
+    public static double FORWARD_OFFSET = -5.22; // in; offset of the lateral wheel
 
 
-    public static double X_MULTIPLIER = 1.0827067669; // Multiplier in the X direction
-    public static double Y_MULTIPLIER = 1.0810810811; // Multiplier in the Y direction
+    public static double X_MULTIPLIER = 1.04; // Multiplier in the X direction
+    public static double Y_MULTIPLIER = 1.07; // Multiplier in the Y direction
 
     private Encoder leftEncoder, rightEncoder, frontEncoder;
 
@@ -52,7 +52,7 @@ public class StandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer 
         lastEncPositions = lastTrackingEncPositions;
         lastEncVels = lastTrackingEncVels;
 
-        leftEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "leftFront"));
+        leftEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "leftBack"));
         rightEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "rightFront"));
         frontEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "rightBack"));
 
