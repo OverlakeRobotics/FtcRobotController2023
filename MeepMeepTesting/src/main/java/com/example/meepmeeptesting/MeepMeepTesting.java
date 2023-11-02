38d499cafce4a86fdab08ca4edf8bcdffe5d3136
dashhyphenminus <robertblendea@outlook.com>
2023-11-01 23:05:26 -0700

    Install MeepMeep

---
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -0,0 +1,32 @@
+package com.example.meepmeeptesting;
+
+import static com.example.meepmeeptesting.GamePositions.*;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+
+public class MeepMeepTesting {
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(800);
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(30, 30, Math.toRadians(180), Math.toRadians(180), 15)
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(BLUE_START_POS_1)
+                                .splineToSplineHeading(BLUE_OBJECT_POS_1, Math.toRadians (0))
+                                .waitSeconds(1)
+                                .splineToSplineHeading(BLUE_BACKDROP_LEFT, Math.toRadians(0))
+                                .build()
+                );
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_OFFICIAL)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
+    }
+}
